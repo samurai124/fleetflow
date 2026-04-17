f262437174638de304e4b5f774fc78851ae4e560
Hamza <hamzazaidi253@gmail.com>
2026-04-17 10:11:34 +0100

    ok

---
+++ src/main/resources/db.migration/V1__init.sql
@@ -0,0 +1,34 @@
+CREATE TABLE client (
+                        id BIGINT PRIMARY KEY AUTO_INCREMENT,
+                        nom VARCHAR(100) NOT NULL,
+                        email VARCHAR(150) NOT NULL UNIQUE,
+                        ville VARCHAR(100),
+                        telephone VARCHAR(20)
+);
+
+CREATE TABLE vehicules (
+                          id BIGINT PRIMARY KEY AUTO_INCREMENT,
+                          matricule VARCHAR(50) NOT NULL UNIQUE,
+                          type VARCHAR(50),
+                          capacite DOUBLE NOT NULL,
+                          statut VARCHAR(50)
+);
+
+CREATE TABLE chauffeurs (
+                           id BIGINT PRIMARY KEY AUTO_INCREMENT,
+                           nom VARCHAR(100) NOT NULL,
+                           telephone VARCHAR(20),
+                           permis_type VARCHAR(50),
+                           disponible BOOLEAN
+);
+
+CREATE TABLE livraisons (
+                           id BIGINT PRIMARY KEY AUTO_INCREMENT,
+                           date_livraison DATE,
+                           adresse_depart VARCHAR(255),
+                           adresse_destination VARCHAR(255),
+                           statut VARCHAR(50),
+                           client_id BIGINT,
+                           vehicule_id BIGINT,
+                           chauffeur_id BIGINT
+);
